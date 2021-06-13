89a304367b9e3f5b37980e25483a05afd823ad47
villirion <75848800+villirion@users.noreply.github.com>
2021-06-13 17:31:20 +0200

    Create GetEigenvector.m

---
+++ GetEigenvector.m
@@ -0,0 +1,18 @@
+function [e,v]=GetEigenvector(refl,retainE)
+A=refl*refl';
+
+if(nargin==1)
+    retainE=6;
+end
+
+[e,v] = eig(A);
+
+v=diag(v);
+v=v(end-retainE+1:end);
+e=e(:,end-retainE+1:end);
+
+%v=flipdim(v,1);
+%e=flipdim(e,2);
+
+v=flip(v,1);
+e=flip(e,2);
