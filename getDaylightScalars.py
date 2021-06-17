68791ab1d8528364318d092a2f9d40c4feabfda0
villirion <75848800+villirion@users.noreply.github.com>
2021-06-17 21:40:34 +0200

    Create getDaylightScalars.py

---
+++ getDaylightScalars.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def getDaylightScalars(CCT):
+
+    if(CCT>=4000 and CCT<=7000):
+        xD = -4.607*10**9/(CCT**3) + 2.9678*10**6/(CCT**2) + 0.09911*10**3/CCT + 0.244063
+    else:
+        xD = -2.0064*10**9/(CCT**3) + 1.9018*10**6/(CCT**2) + 0.24748*10**3/CCT + 0.23704
+
+    yD = -3*xD**2 + 2.87*xD - 0.275
+
+    M1 = (-1.3515-1.7703*xD+5.9114*yD)/(0.0241+0.2562*xD -.7341*yD)
+
+    M2 = (0.03-31.4424*xD+30.0717*yD)/(0.0241+0.2562*xD -.7341*yD)
+
+    daylightScalars  = np.loadtxt('daylightScalars.txt')
+
+    S0 = daylightScalars[:,1]
+    S1 = daylightScalars[:,2]
+    S2 = daylightScalars[:,3]
+
+    SD=S0+M1*S1+M2*S2
+
+    return SD
