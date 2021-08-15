c645eca5dbb47597e084428bcb62f6ac3a0048ac
villirion <75848800+villirion@users.noreply.github.com>
2021-08-15 17:20:11 +0200

    Update Kim_Weyrich_Kautz.py

--- Kim_Weyrich_Kautz.py
+++ Kim_Weyrich_Kautz.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Kim_Weyrich_Kautz(X,Y,Z, Xw,Yw,Zw, La):
+def Kim_Weyrich_Kautz(X,Y,Z, Xw,Yw,Zw, La, E="LCD"):
     
     pi = np.pi
     
@@ -19,6 +19,7 @@ def Kim_Weyrich_Kautz(X,Y,Z, Xw,Yw,Zw, La):
     Lp = (L**nc)/(L**nc + La**nc)
     Mp = (M**nc)/(M**nc + La**nc)
     Sp = (S**nc)/(S**nc + La**nc)
+    
     #question does Aw is with Lwp or just Lw 
     Lwp = (Lw**nc)/(Lw**nc + La**nc)
     Mwp = (Mw**nc)/(Mw**nc + La**nc)
@@ -28,20 +29,21 @@ def Kim_Weyrich_Kautz(X,Y,Z, Xw,Yw,Zw, La):
     A = (40*Lp+20*Mp+Sp)/61
     
     Aw = (40*Lwp+20*Mwp+Swp)/61
-    
+
     # (4) & (5)
     Aj = 0.89 ; Bj = 0.24 ; Oj = 0.65 ; nj = 3.65
     Jp = ((-((A/Aw)-Bj)*Oj**nj)/((A/Aw)-Bj-Aj))**(1/nj)
-
+    
     # (6)
-
-    # A value of E = 1.0 corresponds to a high-luminance LCD display, transparent
-    # advertising media yield E = 1.2175, CRT displays are E = 1.4572,
-    # and reflective paper is E = 1.7526. These parameters were derived
-    # from the LUTCHI data set as well as ours.
-    
-    E = 1 #for test question how changing it
-    
+    if E == "LCD":
+        E = 1.0
+    if E == "media":
+        E = 1.2175
+    if E == "CRT":
+        E = 1.4572
+    if E == "paper":
+        E = 1.7526
+        
     J = 100*(E*(Jp-1)+1)
 
     # (7)
@@ -53,18 +55,18 @@ def Kim_Weyrich_Kautz(X,Y,Z, Xw,Yw,Zw, La):
 
     # (9)
     b = (1/9)*(Lp+Mp-2*Sp)
-
+    
     # (10) & (11)
     Ak = 456.5 ; nk = 0.62 ; Am = 0.11 ; Bm = 0.61
     C = Ak*(np.sqrt(a**2+b**2)**nk)
-    M = C*(Am*np.log(10)*Lw+Bm) #verifier log 
-
+    M = C*(Am*np.log10(Lw)+Bm) #verifier log 
+    
     # (12)
     s = 100*np.sqrt(M/Q)
 
     # (13)
-    h = (180/pi)*np.tan(b/a)**(-1)
-    
+    h = (180/pi)*np.arctan(b/a)
+
     print("Lightness : ", J, "\nBrightness : ", Q, "\nChroma : ", C, 
           "\nColourfulness : ", M,"\nSaturation : ", S, "\nHue angle : ", h)
 
