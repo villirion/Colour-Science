1c587b8997ba54e14741b1cc0923e4fd93755d0c
villirion <75848800+villirion@users.noreply.github.com>
2021-08-15 17:21:04 +0200

    Update Kim_Weyrich_Kautz_inverse.py

--- Kim_Weyrich_Kautz_inverse.py
+++ Kim_Weyrich_Kautz_inverse.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La):
+def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La, E="LCD"):
     
     Aj = 0.89 ; Bj = 0.24 ; Oj = 0.65 ; nj = 3.65
     Ak = 456.5 ; nk = 0.62 ; Am = 0.11 ; Bm = 0.61
     nc = 0.57; nq = 0.1308; pi = np.pi
-    E = 1 #for test question how changing it
+   
+    if E == "LCD":
+        E = 1.0
+    if E == "media":
+        E = 1.2175
+    if E == "CRT":
+        E = 1.4572
+    if E == "paper":
+        E = 1.7526
     
     MATRIX_XYZ_TO_LMS = np.array([
         [0.38971, 0.68898, -0.07868],
@@ -23,16 +31,17 @@ def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La):
     Aw = Aw.item(0)
     
     J = Q/(Lw)**nq
-
-    Jp = (J/100 - 1)/(E + 1)
+    
+    Jp = (J/100 - 1)/E + 1
     Jp = Jp.item(0)
     
-    A = Aw*((Aj * Jp**nj)/(Jp**nj + Aj**nj) + Bj)
-
-    C = M/(Am*np.log(10)*Lw + Bm)
+    A = Aw * ((Aj * (Jp**nj)) / ((Jp**nj) + (Oj**nj)) + Bj)
     
-    a = np.cos(pi*h/180) * (C/Ak)**(1/nk)
-    b = np.sin(pi*h/180) * (C/Ak)**(1/nk)
+    C = M/(Am*np.log10(Lw) + Bm)
+    
+    #ajout du moins pour des raisons obscurs ?
+    a = -( np.cos(pi*h/180) * (C/Ak)**(1/nk))
+    b = -( np.sin(pi*h/180) * (C/Ak)**(1/nk))
     
     Mt = np.array([
         [1.0000, 0.3215, 0.2053],
@@ -48,7 +57,8 @@ def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La):
 
     X,Y,Z = np.linalg.inv(MATRIX_XYZ_TO_LMS) @ np.array([L,M,S])
     
-    return X,Y,Z
+    return X.item(0), Y.item(0), Z.item(0)
+
 
 """
 #input : 
