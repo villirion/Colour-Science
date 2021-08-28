84f5a35f5a4eca3283bde44d02784192f00aa37d
villirion <75848800+villirion@users.noreply.github.com>
2021-08-28 14:15:47 +0200

    Update Jzazbz.py

--- Jzazbz.py
+++ Jzazbz.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Jzazbz(Xd65,Yd65,Zd65,Yb,Yw,La,c):
+def Jzazbz(XYZd65,Yb,Yw,La,c):
     
     def get_alpha(Yb,Yw):
         alpha = 1.2 + 1.7*np.sqrt(Yb/Yw)
@@ -11,7 +11,6 @@ def Jzazbz(Xd65,Yd65,Zd65,Yb,Yw,La,c):
         FL = La*(k**4) + 0.1*((1-(k**4))**2)*((5*La)**(1/3))
         return FL
     
-    # use for (5)
     def get_val(h): 
         #  Red        Yellow       Green       Blue        Red
         h1 = 33.44; h2 = 88.29; h3 = 146.30; h4 = 238.36; h5 = 393.44
@@ -31,66 +30,53 @@ def Jzazbz(Xd65,Yd65,Zd65,Yb,Yw,La,c):
         if h4<=hp<h5:
             return h4,hp,h5,H4,e4,e5
     
-    # variable
-    b = 1.16; g = 0.7; c1 = 3424/(2**12)
-    c2 = 2413/(2**7); c3 = 2392/(2**7)
-    n = 2610/(2**14); p = 1.7*2523/(2**5)
+    b = 1.16; g = 0.7; c1 = (3424)/(2**12)
+    c2 = (2413)/(2**7); c3 = (2392)/(2**7)
+    n = (2610)/(2**14); p = (1.7*2523)/(2**5)
     pi = np.pi
     
-    #(1) 
-    Xdp65,Ydp65 = np.array([[b*Xd65],[g*Yd65]]) - np.array([[(b-1)*Zd65],[(g-1)*Xd65]])
+    Xdp65,Ydp65 = np.array([[b*XYZd65[0]],[g*XYZd65[1]]]) - np.array([[(b-1)*XYZd65[2]],[(g-1)*XYZd65[0]]])
     
-    #(2)
     Mt = np.array([
         [0.41478972, 0.579999, 0.0146480],
         [-0.2015100, 1.120649, 0.0531008], 
         [-0.0166008, 0.264800, 0.6684799],
     ])
-
-    R,G,B = Mt @ np.array([Xdp65, Ydp65, [Zd65]])
     
-    #(3) 
-    Rp = ((c1+c2*(R/10000)**n)/(1+c3*(R/10000)**n))**p
-    Gp = ((c1+c2*(G/10000)**n)/(1+c3*(G/10000)**n))**p
-    Bp = ((c1+c2*(B/10000)**n)/(1+c3*(B/10000)**n))**p
+    RGB = Mt @ np.array([Xdp65, Ydp65, [XYZd65[2]]])
+    
+    RGBp = ( ( c1 + c2*(RGB/10000)**n ) / ( 1 + c3*(RGB/10000)**n ) )**p
 
-    # (4) 
     Mt2 = np.array([
-        [0,        1,         0,],
-        [3.524000, -4.066708, 0.542708,],
+        [0,        1,         0],
+        [3.524000, -4.066708, 0.542708],
         [0.199076, 1.096799, -1.295875],
     ])
     
-    I,az,bz  = Mt2 @ np.array([Rp, Gp, Bp])
-    
-    #(5)
+    I,az,bz  = Mt2 @ RGBp
+
     h = np.arctan(bz.item(0)/az.item(0)) # 0<=h<=360
+    h = -(h*180/pi)
     hi, hp, hip, Hi, ei, eip = get_val(h)
 
-    # (6) 
     et = 1.01+np.cos(1.55+(hp*pi)/180)
     H = Hi + (100*(hp-hi)/ei)/(((hp-hi)/ei)+((hip-hp)/eip))
 
-    # (7) La en input yb yw pas sur de ce que c'est
     FL = get_FL(La) 
     alpha = get_alpha(Yb,Yw)
 
-    # c en input
     Q = 192 * I.item(0)**1.17 * np.sqrt(FL/c)
     
-    # (8) Qn = Q*n ?
     J = 89*(Q/(Q*n))**(0.72*c*alpha)
 
-    # (9) 
     C = (1/n)**0.074*(az.item(0)**2+bz.item(0)**2)**0.37*(et)**0.067
 
-    # (10) 
     M = 1.42*C*(FL)**0.25
 
-    # (11) 
     S = 100*(M/Q)**0.5
     
-    print("Lightness : ", J, "\nBrightness : ", Q, "\nChroma : ", C, "\nColourfulness : ", M, "\nSaturation : ", S)
+    # print("Lightness : ", J, "\nBrightness : ", Q, "\nChroma : ", C, 
+    #"\nColourfulness : ", M, "\nSaturation : ", S, "\nHue angle :", h)
 
     return J,Q,C,M,S,h
   
