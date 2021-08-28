b48231ab53b2ea4fb3762e2a13eec92667273a4d
villirion <75848800+villirion@users.noreply.github.com>
2021-08-28 14:16:55 +0200

    Update Kim_Weyrich_Kautz_inverse.py

--- Kim_Weyrich_Kautz_inverse.py
+++ Kim_Weyrich_Kautz_inverse.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La, E="LCD"):
+def Kim_Weyrich_Kautz_inverse(XYZw, Q, M, h, La, E="LCD"):
     
     Aj = 0.89 ; Bj = 0.24 ; Oj = 0.65 ; nj = 3.65
     Ak = 456.5 ; nk = 0.62 ; Am = 0.11 ; Bm = 0.61
@@ -21,25 +21,21 @@ def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La, E="LCD"):
         [0.00000, 0.00000, 1.00000],
     ])
     
-    Lw,Mw,Sw = MATRIX_XYZ_TO_LMS @ np.array([[Xw],[Yw],[Zw]])
+    LMSw = MATRIX_XYZ_TO_LMS @ XYZw
     
-    Lwp = (Lw**nc)/(Lw**nc + La**nc)
-    Mwp = (Mw**nc)/(Mw**nc + La**nc)
-    Swp = (Sw**nc)/(Sw**nc + La**nc)
+    LMSwp = (LMSw**nc)/(LMSw**nc + La**nc)
     
-    Aw = (40*Lwp+20*Mwp+Swp)/61
-    Aw = Aw.item(0)
-    
-    J = Q/(Lw)**nq
+    Aw = (40*LMSwp[0]+20*LMSwp[1]+LMSwp[2])/61
+   
+    J = Q/(LMSw[0])**nq
     
     Jp = (J/100 - 1)/E + 1
-    Jp = Jp.item(0)
     
     A = Aw * ((Aj * (Jp**nj)) / ((Jp**nj) + (Oj**nj)) + Bj)
     
-    C = M/(Am*np.log10(Lw) + Bm)
+    C = M/(Am*np.log10(LMSw[0]) + Bm)
     
-    #ajout du moins pour des raisons obscurs ?
+    # minus a and b to match the test but may be different with additional tests
     a = -( np.cos(pi*h/180) * (C/Ak)**(1/nk))
     b = -( np.sin(pi*h/180) * (C/Ak)**(1/nk))
     
@@ -49,15 +45,12 @@ def Kim_Weyrich_Kautz_inverse(Xw,Yw,Zw, Q, M, h, La, E="LCD"):
         [1.0000, -0.1568, -4.4904],
     ])
 
-    Lp,Mp,Sp = Mt @ np.array([[A],a,b])
+    LMSp = Mt @ np.array([A,a,b])
     
-    L = ((-(La**nc)*Lp)/(Lp-1))**(1/nc)
-    M = ((-(La**nc)*Mp)/(Mp-1))**(1/nc)
-    S = ((-(La**nc)*Sp)/(Sp-1))**(1/nc)
-
-    X,Y,Z = np.linalg.inv(MATRIX_XYZ_TO_LMS) @ np.array([L,M,S])
+    LMS = ((-(La**nc)*LMSp)/(LMSp-1))**(1/nc)
+    XYZ = np.linalg.inv(MATRIX_XYZ_TO_LMS) @ LMS
     
-    return X.item(0), Y.item(0), Z.item(0)
+    return XYZ
 
 
 """
